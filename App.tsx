270480e5247c19baffb4ac5fc3e121b182e074ef
saszaCodes <sasza.x.ia@gmail.com>
2022-10-03 12:26:12 +0200

    add settings context

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
+import { SettingsProvider } from "./contexts/SettingsContext";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { TimeTrackerProvider } from "./contexts/TimeTrackerContext";
 import { MainNavigator } from "./navigators/MainNavigator";
 
 export default function App() {
   return (
-    <TimeTrackerProvider>
-      <ThemeProvider>
-        <MainNavigator />
-      </ThemeProvider>
-    </TimeTrackerProvider>
+    <SettingsProvider>
+      <TimeTrackerProvider>
+        <ThemeProvider>
+          <MainNavigator />
+        </ThemeProvider>
+      </TimeTrackerProvider>
+    </SettingsProvider>
   );
 }
