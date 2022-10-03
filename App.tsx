8ead0ca77a7246eae8b1d9081ba5a1004f48ec67
saszaCodes <sasza.x.ia@gmail.com>
2022-10-03 12:21:08 +0200

    add navigation

--- App.tsx
+++ App.tsx
@@ -1,26 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { TimeTrackerProvider } from "./contexts/TimeTrackerContext";
+import { MainNavigator } from "./navigators/MainNavigator";
 
 export default function App() {
   return (
     <TimeTrackerProvider>
       <ThemeProvider>
-        <View style={styles.container}>
-          <Text>Open up App.tsx to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
+        <MainNavigator />
       </ThemeProvider>
     </TimeTrackerProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
