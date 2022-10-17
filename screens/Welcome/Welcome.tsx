e8747dcc618710c7b15e4ff314b92179dc967528
saszaCodes <sasza.x.ia@gmail.com>
2022-10-17 12:49:04 +0200

    sort trackers by date

--- screens/Welcome/Welcome.tsx
+++ screens/Welcome/Welcome.tsx
@@ -5,6 +5,7 @@ import { ScrollView } from "react-native-gesture-handler";
 import { reminderOptions } from "../../components/NewTimeTrackerForm/components/Reminders";
 import { useTimeTrackerContext } from "../../contexts/TimeTrackerContext";
 import { MainNavigatorParams } from "../../types/types";
+import moment from "moment";
 
 // TODO: fix type
 export const WelcomeScreen = ({
@@ -12,26 +13,55 @@ export const WelcomeScreen = ({
 }: DrawerScreenProps<MainNavigatorParams>) => {
   const { trackers } = useTimeTrackerContext();
 
-  const generateTrackers = useCallback(() => {
-    return trackers.map((tracker, i) => {
-      return (
-        <View key={i}>
-          <Text>{tracker.name}</Text>
-          <Text>{tracker.finishDate}</Text>
-          <Text>{tracker.description}</Text>
-          {tracker.reminders
-            ? tracker.reminders?.map((key, i) => (
-                <Text key={i}>{reminderOptions[key].label}</Text>
-              ))
-            : null}
-        </View>
-      );
-    });
-  }, [trackers]);
+  const generateTrackers = useCallback(
+    (params?: { startDate: number; endDate: number }) => {
+      return trackers.map((tracker, i) => {
+        const { name, finishDate, description, reminders } = tracker;
+        if (params) {
+          const { startDate: listStartDate, endDate: listEndDate } = params;
+          if (listStartDate >= listEndDate) return null;
+          if (listStartDate > finishDate || finishDate > listEndDate)
+            return null;
+        }
+        return (
+          <View key={i}>
+            <Text>{name}</Text>
+            <Text>{finishDate}</Text>
+            <Text>{description}</Text>
+            {reminders
+              ? reminders?.map((key, i) => (
+                  <Text key={i}>{reminderOptions[key].label}</Text>
+                ))
+              : null}
+          </View>
+        );
+      });
+    },
+    [trackers]
+  );
 
   return (
     <ScrollView>
-      {generateTrackers()}
+      <Text>Today</Text>
+      {generateTrackers({
+        startDate: moment().valueOf(),
+        endDate: moment().endOf("day").valueOf(),
+      })}
+      <Text>Tommorow</Text>
+      {generateTrackers({
+        startDate: moment().add(1, "days").startOf("day").valueOf(),
+        endDate: moment().add(1, "days").endOf("day").valueOf(),
+      })}
+      <Text>Missed</Text>
+      {generateTrackers({
+        startDate: 0,
+        endDate: moment().valueOf(),
+      })}
+      <Text>Future</Text>
+      {generateTrackers({
+        startDate: moment().add(1, "days").endOf("day").valueOf(),
+        endDate: Infinity,
+      })}
       <Button
         onPress={() => navigation.navigate("AddTracker")}
         title="Add new tracker"
